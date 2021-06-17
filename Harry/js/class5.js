bb0613152d0769978de65dae5ca18c0d393b5043
Proshenzit <37993972+Proshenzit@users.noreply.github.com>
2021-06-17 05:32:08 -0700

    Add files via upload

---
+++ Harry/js/class5.js
@@ -0,0 +1,54 @@
+//type conversion
+
+console.log("welcome  class5");
+
+let myvar;
+
+myvar = String(34);
+
+console.log(myvar,(typeof myvar));
+
+let booleanVar = String(true);
+
+console.log(booleanVar,(typeof booleanVar));
+
+let date = String (new Date());
+
+console.log(date, (typeof date));
+
+let arr =  String[1,2,3,4,5];
+ console.log(arr, (typeof arr));
+
+
+let stri = Number("3214");
+
+
+stri = Number("32d14");
+
+
+ stri = Number(true);
+
+ stri = Number([12,2,3,5,5,56,7]);
+
+ 
+
+// console.log(stri,(typeof stri));
+
+let roy =parseFloat('34.123456789');
+
+console.log(roy.toFixed(5),(typeof roy));
+
+
+
+// type corecion
+
+let mystr = Number("645");
+
+let mynum=152;
+console.log(mystr+mynum);
+
+
+
+
+
+
